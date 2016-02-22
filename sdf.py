47cf89e0b60f758dd6bce9126d2d7dcba370c02d
Ivan <ivanho_yo@hotmail.com>
2016-02-22 08:48:39 -0500

    pep 8 compliancy
    with flake8

--- sdf.py
+++ sdf.py
@@ -2,7 +2,8 @@ import datetime
 from datetime import date
 import calendar
 import tkinter as tk
-# from tkinter import BOTH, END
+from tkinter import END
+# from tkinter import BOTH
 import MySQLdb
 
 from collections import defaultdict
@@ -10,7 +11,7 @@ from collections import defaultdict
 # kivy stuff
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.graphics import *
+from kivy.graphics import InstructionGroup, Rectangle, Color
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty
@@ -19,7 +20,8 @@ import sys
 from kivy.core.window import Window
 from kivy.clock import Clock
 
-class CalGame(GridLayout): # main class
+
+class CalGame(GridLayout):  # main class
     def kivycalpop(self):
         # same draw method, just in kivy instead of tkinter
         gridindex = dofotm() + 1
@@ -35,7 +37,7 @@ class CalGame(GridLayout): # main class
                 self.add_widget(ew)
                 y = y + 1
             else:
-                self.add_widget(DayBtn(text=str(currday),input=binddate))
+                self.add_widget(DayBtn(text=str(currday), input=binddate))
                 currday = currday + 1
 
     def makemarks(self):
@@ -52,18 +54,20 @@ class CalGame(GridLayout): # main class
 
         self.canvas.add(ig1)
 
-class EW(Widget): # empty widget class
+
+class EW(Widget):  # empty widget class
 
     # TODO: dummy var and method to be consistent with DayBtn class
     input = ObjectProperty
 
-    def drawnew(self,job):
+    def drawnew(self, job):
         pass
 
-class DayBtn(Button): # day button class
+
+class DayBtn(Button):  # day button class
     input = ObjectProperty()
-    background_color = (1,1,1,1)
-    text_size = (100,100)
+    background_color = (1, 1, 1, 1)
+    text_size = (100, 100)
     halign = 'right'
     valign = 'top'
     font_size = 32
@@ -71,23 +75,24 @@ class DayBtn(Button): # day button class
     def on_release(self):
         ntf(self.input)
 
-    def drawnew(self,job):
+    def drawnew(self, job):
         # TODO: pass into function better, maybe dont need job
         #       datetime.datetime in DayBtn
         # doesnt actually draw but only add instrucions.
-        ig1.add(Color(1,1,0,0.5))
-        ig1.add(Rectangle(size=(self.width-40,\
-                                self.height-40),\
-                           pos=(self.x+20,\
-                                self.y+20)))
+        ig1.add(Color(1, 1, 0, 0.5))
+        ig1.add(Rectangle(size=(self.width-40,
+                                self.height-40),
+                          pos=(self.x+20,
+                               self.y+20)))
         # calculate timereq indicator stuff...
         secwidth = Window.width/7./180000.
-        #timeReq width
+        # timeReq width
         trw = job.timeReq*secwidth
-        ig1.add(Rectangle(size=(trw+20,\
-                                self.height-80),\
-                           pos=(self.x-trw,\
-                                self.y+20)))
+        ig1.add(Rectangle(size=(trw+20,
+                                self.height-80),
+                          pos=(self.x-trw,
+                               self.y+20)))
+
 
 class CalApp(App):
 
@@ -101,28 +106,29 @@ class CalApp(App):
             Rectangle(pos=main.pos, size=Window.size)
 
         main.kivycalpop()
-        Clock.schedule_once(self.delayed, 0.5) #TODO refine delay
+        Clock.schedule_once(self.delayed, 0.5)  # TODO: refine delay
         return main
 
 # dodwj = {} #dictionary of days with jobs
 dodwj = defaultdict(dict)
 ig1 = InstructionGroup()
 
+
 class Database:
 
-    host    = "localhost"
-    user    = "root"
-    passwd  = "asdf"
-    db      = "test"
+    host = "localhost"
+    user = "root"
+    passwd = "asdf"
+    db = "test"
 
     def __init__(self):
-        self.connection = MySQLdb.connect( host = self.host,
-                                           user = self.user,
-                                           passwd = self.passwd,
-                                           db = self.db )
+        self.connection = MySQLdb.connect(host=self.host,
+                                          user=self.user,
+                                          passwd=self.passwd,
+                                          db=self.db)
 
     def query(self, q):
-        cursor = self.connection.cursor( MySQLdb.cursors.DictCursor )
+        cursor = self.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute(q)
 
         return cursor.fetchall()
@@ -143,14 +149,18 @@ class Database:
     def delitem_byname(self, name):
         pass
 
+
 class Job:
-    def __init__(self, name, year, month, day, hour, minute, second, timereq, job_id):
+    def __init__(self, name,
+                 year, month, day, hour, minute, second,
+                 timereq, job_id):
         self.name = name
         self.timeReq = timereq
 #       doBy is the date of occurence
-#       
 #                                     year  m  d   h   m   s   mics  tz
-        self.doBy = datetime.datetime(year, month, day, hour, minute, second, 0, None)
+        self.doBy = datetime.datetime(year, month, day,
+                                      hour, minute, second,
+                                      0, None)
         # self.doBy = datetime.datetime(2016, 1, 27, 20, 46, 43, 0, None)
         self.job_id = job_id
 
@@ -161,9 +171,13 @@ class Job:
         return self.timeUntil
 
 # time left (calculatd from a specific time)
-    def timeLeftFromTime(self, year, month, day, hour, minute, second, microsecond, tzinfo):
+    def timeLeftFromTime(self, year, month, day,
+                         hour, minute, second,
+                         microsecond, tzinfo):
         self.timeUntil = self.doBy - \
-                datetime.datetime.today(year, month, day, hour, minute, second, microsecond, tzinfo)
+                datetime.datetime.today(year, month, day,
+                                        hour, minute, second,
+                                        microsecond, tzinfo)
         return self.timeUntil
 
     def __str__(self):
@@ -177,86 +191,106 @@ class Job:
         self.outstr += str(self.doBy.hour) + ":"
         self.outstr += str(self.doBy.minute) + ":"
         self.outstr += str(self.doBy.second) + " timeReq="
-        #self.outstr += str(self.doBy.microsecond) + " "
-        #self.outstr += str(self.doBy.tzinfo)
+        # self.outstr += str(self.doBy.microsecond) + " "
+        # self.outstr += str(self.doBy.tzinfo)
         self.outstr += str(self.timeReq)
 
         return self.outstr
 
 #####################################
+
+
 def initdbclass():
-    q = "DELETE FROM wt" # clear table before doing anything
+    q = "DELETE FROM wt"  # clear table before doing anything
     db.query(q)
 
     q = """
-    INSERT INTO wt VALUES('laundry','180000', STR_TO_DATE('2016/02/20 20:46:43', '%Y/%m/%d %T'), NULL);
+    INSERT INTO wt VALUES('laundry','180000',
+                          STR_TO_DATE('2016/02/20 20:46:43',
+                                      '%Y/%m/%d %T'), NULL);
     """
     db.query(q)
 
     q = """
-    INSERT INTO wt VALUES('something new on same date','8400', STR_TO_DATE('2016/02/27 23:46:43', '%Y/%m/%d %T'), NULL);
+    INSERT INTO wt VALUES('something new on same date','8400',
+                          STR_TO_DATE('2016/02/27 23:46:43',
+                                      '%Y/%m/%d %T'), NULL);
     """
     db.query(q)
 
     q = """
-    INSERT INTO wt VALUES('database project','0', STR_TO_DATE('2016/02/15 08:00:00', '%Y/%m/%d %T'), NULL);
+    INSERT INTO wt VALUES('database project','0',
+                          STR_TO_DATE('2016/02/15 08:00:00',
+                                      '%Y/%m/%d %T'), NULL);
     """
     db.query(q)
 
     q = """
-    INSERT INTO wt VALUES('paper','18000', STR_TO_DATE('2016/02/08 14:00:00', '%Y/%m/%d %T'), NULL);
+    INSERT INTO wt VALUES('paper','18000',
+                          STR_TO_DATE('2016/02/08 14:00:00',
+                                      '%Y/%m/%d %T'), NULL);
     """
     db.query(q)
 
     q = """
-    INSERT INTO wt VALUES('csc club meeting','180000', STR_TO_DATE('2016/02/08 11:00:00', '%Y/%m/%d %T'), NULL);
+    INSERT INTO wt VALUES('csc club meeting','180000',
+                          STR_TO_DATE('2016/02/08 11:00:00',
+                                      '%Y/%m/%d %T'), NULL);
     """
     db.query(q)
     db.connection.commit()
 
 
-def dofotm(): # day of first of this month
-    return date(datetime.datetime.today().year, datetime.datetime.today().month, 1).weekday()
+def dofotm():  # day of first of this month
+    return date(datetime.datetime.today().year,
+                datetime.datetime.today().month, 1).weekday()
 
-def dofotm_specific(y, m): # day of first of a specific month
+
+def dofotm_specific(y, m):  # day of first of a specific month
     return date(y, m, 1).weekday()
 
-def ditm(): # days in this month
-    return calendar.monthrange(datetime.datetime.today().year, datetime.datetime.today().month)[1]
 
-def ditm_specific(y, m): # days in this month
+def ditm():  # days in this month
+    return calendar.monthrange(datetime.datetime.today().year,
+                               datetime.datetime.today().month)[1]
+
+
+def ditm_specific(y, m):  # days in this month
     return calendar.monthrange(y, m)[1]
 
-def ntf(arg): # new test function
+
+def ntf(arg):  # new test function
     print("button pressed, ntf(" + str(arg) + ") invoked")
     for item in dodwj[arg]:
         print(dodwj[arg][item])
+
 
 def tf(arg):
     print("button pressed, tf(" + str(arg) + ") invoked")
     joblistbox.delete(0, END)
     for item in dodwj[arg]:
         joblistbox.insert(END, dodwj[arg][item])
-    #print(dodwj[arg])
+    # print(dodwj[arg])
+
 
 def calpop():
-# populates a grid with the days of the month in a button format
+    # populates a grid with the days of the month in a button format
     gridindex = dofotm() + 1
     currday = 1
-    daywidth = 32 # 32 pixels width
-    calwidth = daywidth * 7 # 7 days in a week
-    #calheight = daywidth * 5 # max 5 weeks in a month?
+    daywidth = 32  # 32 pixels width
+    calwidth = daywidth * 7  # 7 days in a week
+    # calheight = daywidth * 5 # max 5 weeks in a month?
 
-    
-    calwidth = 2 # temp calwidth, 2 is width based on characters
-    for x in range(0, 9): # will just break after done? max=99 is ok?
+    calwidth = 2  # temp calwidth, 2 is width based on characters
+    for x in range(0, 9):  # will just break after done? max=99 is ok?
         if (currday == ditm()):
             break
         for y in range(0, 7):
             y = gridindex
             mnth = datetime.datetime.today().month
             binddate = datetime.date(2016, mnth, currday)
-            btn1 = tk.Button(root, width=calwidth, text=currday, command=lambda j=binddate: tf(j))
+            btn1 = tk.Button(root, width=calwidth, text=currday,
+                             command=lambda j=binddate: tf(j))
             btn1.grid(row=x, column=y)
             if (currday == ditm()):
                 break
@@ -265,10 +299,10 @@ def calpop():
             if (gridindex == 7):
                 gridindex = 0
                 break
-    
+
 
 def refreshjoblist():
-    q="""
+    q = """
     SELECT * from wt;
     """
     dbdata = db.query(q)
@@ -283,42 +317,42 @@ def refreshjoblist():
         minutestr = int(datetimestr[14:16])
         secondstr = int(datetimestr[17:19])
         timereq = item['timereq']
-        joblist.append(Job(item['name'], yearstr, monthstr, daystr, hourstr, minutestr, secondstr, timereq, item['job_id']))
+        joblist.append(Job(item['name'], yearstr, monthstr, daystr,
+                       hourstr, minutestr, secondstr,
+                       timereq, item['job_id']))
 
-    matches = 0 # dont even need this anymore
+    matches = 0  # dont even need this anymore
 
     for item in joblist:
         if (item.doBy.date() in doBylist):
             matches = matches + 1
         else:
             doBylist.append(item.doBy.date())
-    
-    
+
     newlist = [x for x in doBylist if x == joblist[0].doBy.date()]
-    print("sdf " + str(newlist)) # newlist contains unique dates?
+    print("sdf " + str(newlist))  # newlist contains unique dates?
     i = 1
     global dodwj
     for item in joblist:
         if(item.doBy.date() in dodwj):
             i = i + 1
-            dodwj[item.doBy.date()][i] = item # dict can override
+            dodwj[item.doBy.date()][i] = item  # dict can override
         else:
-            dodwj[item.doBy.date()][i] = item # dict can override
+            dodwj[item.doBy.date()][i] = item  # dict can override
         i = 1
     print("printing dodwj:")
 
     # to access list of jobs by date... # TIP
-    #for item in dodwj:
+    # for item in dodwj:
     #    for jobitem in dodwj[item]:
     #            #print(dodwj[item][jobitem])
 
     # - get the dates from doBylist (this contains unique dates)
     # - traverse list of jobs. create new array for first occurence of
-    #   a date. add job to that date. for other jobs on that date, add 
+    #   a date. add job to that date. for other jobs on that date, add
     #   them to the created array
 
     # - when populating cal, change color when there is event
-    # 
 
     print("#########################")
     print("refreshed job DB. items: ")
@@ -327,7 +361,7 @@ def refreshjoblist():
         print(item)
 
 if __name__ == "__main__":
-    db = Database() # custom database class (mysql)
+    db = Database()  # custom database class (mysql)
 
     initdbclass()
     joblist = []
@@ -338,7 +372,7 @@ if __name__ == "__main__":
     year = 2016
     month = 1
 
-    #db.additem('wt', 'newdbitem', '3600')
+    # db.additem('wt', 'newdbitem', '3600')
 
     CalApp().run()
     sys.exit()
@@ -346,7 +380,7 @@ if __name__ == "__main__":
     # tkinter stuff
     root = tk.Tk()
     root.title("tkinter window")
-    test1 = tk.Label(root, text = "thistext")
+    test1 = tk.Label(root, text="thistext")
 
 #    f = tk.Frame(root, height=32, width=32)
 #    f.pack_propagate(0)
@@ -357,9 +391,9 @@ if __name__ == "__main__":
 
     joblistbox = tk.Listbox(root, width=35, height=5)
     for item in joblist:
-        joblistbox.insert(END, item.name) # insert in listbox
+        joblistbox.insert(END, item.name)  # insert in listbox
 
-    joblistbox.grid(row=5, columnspan=7) # figure this out
+    joblistbox.grid(row=5, columnspan=7)  # figure this out
 
     calpop()
 
