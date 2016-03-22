f2b58924e591e936ee24a69accb762e53d872ad9
Ivan <ivanho_yo@hotmail.com>
2016-03-22 16:27:30 -0400

    Progress:
    fixed colors not being persistant
    
    can now disable bars for cleaner view via clean variable
    
    added buttons for next and previous month (middle button is
    redraw with current month)
    
    added two items to db in april for testing purposes

--- sdf.py
+++ sdf.py
@@ -18,7 +18,7 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
-from kivy.properties import ObjectProperty, StringProperty
+from kivy.properties import ObjectProperty, StringProperty, NumericProperty
 import sys
 
 from kivy.core.window import Window
@@ -101,6 +101,28 @@ Builder.load_string("""
     #size_hint:(None,None)
     valign: 'top'
 
+<MonthDisplay>:
+    md: md
+    canvas.before:
+        Color:
+            rgba:1,1,1,1
+        Rectangle:
+            pos:(self.pos)
+            size:(self.size)
+
+        Color:
+            rgba:0.4,0.4,0.4,1
+        Rectangle:
+            pos:(self.x+1, self.y+1)
+            size:(self.width-2,self.height-2)
+
+    Label:
+        id: md
+        color:(1,1,1,1)
+        
+        text: self.parent.mnthtext
+
+
 <DayDisplay>:
     DayLabel:
         text:'[color=ffffff]SUN[/color]'
@@ -157,7 +179,7 @@ def rgb256to1(x, y, z):
     return x, y, z, 0.5
 
 class BtnArea(GridLayout):
-    pass
+    date = ObjectProperty()
 
 class CalGame(GridLayout):  # main class
     cl = []
@@ -192,16 +214,18 @@ class CalGame(GridLayout):  # main class
     cl.append(rgb256to1(231, 224, 97))
     cl.append(rgb256to1(234, 194, 81))
     seed = 20
+    dater = ObjectProperty()
 
     ig0 = InstructionGroup() # white background
     ig1 = InstructionGroup()
     ig2 = InstructionGroup()
     ig3 = InstructionGroup()
     ig0.add(Color(1,1,1,1))
-    ig0.add(Rectangle(size=(Window.width,Window.height * 0.971)))
+    ig0.add(Rectangle(size=(Window.width,Window.height * 0.90)))
     # TODO: doesn't fill completely accurately, get better measurements
 
-    def kivycalpop(self):
+    def kivycalpop(self, yr, mnth):
+        self.parent.children[3].md.text = str(self.dater.month) + '/' + str(self.dater.year)
         # for refresh, don't really the next 2 lines need because
         # it will happen later, but the later thing is scheduled
         # 0.01 seconds later because it requires creation. not sure
@@ -213,13 +237,13 @@ class CalGame(GridLayout):  # main class
         for x in range (0,len(self.children)):
             self.remove_widget(self.children[0])
         # this is for the calendar buttons only
-        gridindex = dofotm() + 1
+        gridindex = dofotm_specific(yr, mnth) + 1
         currday = 1
         y = 0
-        yr = arrow.get().year
-        mnth = arrow.get().month
+        # yr = arrow.get(yr, mnth, 1).year
+        # mnth = arrow.get(yr, mnth, 1).month
 
-        while (currday <= ditm()):
+        while (currday <= ditm_specific(yr, mnth)):
             #TODO: change to SPECIFIC MONTH for refresh
             arwdate = arrow.get(yr, mnth, currday, 0,0,0,0, tz.tzlocal())
             arwdate = arwdate.date()
@@ -240,11 +264,11 @@ class CalGame(GridLayout):  # main class
         insgrp.clear()
         pass
 
-    def makemarks(self):
+    def makemarks(self, yr, mnth):
         print('MADE MARKS!')
         self.rm(self.ig1) # removes the insgrp from canvas
         self.rm(self.ig2)
-        getspace(self)
+        getspace(self, yr, mnth)
         self.ig1.clear() # clears the insgrp
         self.ig2.clear()
 
@@ -267,17 +291,15 @@ class CalGame(GridLayout):  # main class
         self.canvas.add(self.ig2) # 2 is job related markers
 
 
-def getspace(self):
+def getspace(self, yr, mnth):
     # get days in this month first
-    ditmvar = ditm()
+    ditmvar = ditm_specific(yr, mnth)
     # check job against each day (in month)
     # this is for the bars. have to check each day
     # to see what jobs need to be done on what days
     currday = 1
-    year = arrow.get().year
-    mnth = arrow.get().month
     while (currday <= ditmvar):
-        datetocheck = arrow.get(year, mnth, currday,0,0,0,0,tz.tzlocal())
+        datetocheck = arrow.get(yr, mnth, currday,0,0,0,0,tz.tzlocal())
         jobstodo = []
 
         for job in joblist:
@@ -301,6 +323,7 @@ def getspace(self):
                 if (datetocheck.floor('day') >= mswd):
                     jobstodo.append(job)
 
+                # why did i do this?
                 if (datetocheck.floor('day').replace(days=1) >= dobyarrow):
                     if job in jobstodo:
                         jobstodo.remove(job)
@@ -496,12 +519,25 @@ def spawnnw(self, jobitems, arwin):
 
 
 class EWB(Button):
+    shift = NumericProperty()
+    yr = NumericProperty()
+    mnth = NumericProperty()
+    # TODO: not same instance
+    def dates(self):
+        self.yr = self.parent.parent.children[1].dater.year
+        self.mnth = self.parent.parent.children[1].dater.month
+
+    def getshift(self):
+        self.parent.parent.children[1].dater = self.parent.parent.children[1].dater.replace(months=+self.shift)
+   
     screeninput = ObjectProperty()
     def delayed(self, dt):
-        self.parent.parent.children[1].makemarks()
+        self.parent.parent.children[1].makemarks(self.yr, self.mnth)
 
     def on_release(self):
-        self.parent.parent.children[1].kivycalpop()
+        self.getshift()
+        self.dates()
+        self.parent.parent.children[1].kivycalpop(self.yr, self.mnth)
         Clock.schedule_once(self.delayed, .01)  # TODO: refine delay
         # screeninput = self.parent.parent.parent.parent
         # self.parent.children[1].makemarks()
@@ -513,7 +549,12 @@ class EWB(Button):
 class DayLabel(Label):  # empty widget class
     pass
 
-class DayDisplay(BoxLayout):  # empty widget class
+class MonthDisplay(BoxLayout):
+    md = ObjectProperty(None)
+    mnthtext = StringProperty()
+    mnthtext = 'test'
+
+class DayDisplay(BoxLayout):
     pass
 
 class EW(Widget):  # empty widget class
@@ -564,14 +605,14 @@ class DayBtn(Button):  # day button class
         # ri = random.randint(0, len(self.parent.cl)-1)
         #self.parent.cl.pop(ri)
 
-        job.clr = self.parent.cl[ri][0],\
-            self.parent.cl[ri][1],\
-            self.parent.cl[ri][2]
+        if (job.clr == None):
+            # dont reroll colors if already have  color
+            print('job clr unset')
+            job.clr = self.parent.cl[ri][0],\
+                self.parent.cl[ri][1],\
+                self.parent.cl[ri][2]
 
-        insgrp.add(Color(self.parent.cl[ri][0],
-                      self.parent.cl[ri][1],
-                      self.parent.cl[ri][2],))
-        # insgrp.add(Color(job.clr))
+        insgrp.add(Color(job.clr[0],job.clr[1],job.clr[2]))
 
         # roundedrect marker to indicate doBy
         if(checkl()):
@@ -587,101 +628,104 @@ class DayBtn(Button):  # day button class
                                  radius=(rad, rad)))
 
         # calculate timereq indicator stuff...
-        secwidth = self.parent.width/7./86400.  # pixels each second
-        # timeReq width, full length required to visualize timereq
-        trw = job.timeReq*secwidth
+        clean = False
+        if not clean:
+            secwidth = self.parent.width/7./86400.  # pixels each second
+            # timeReq width, full length required to visualize timereq
+            trw = job.timeReq*secwidth
 
-        # 0 is sunday, 6 is saturday
-        dayindex = job.doBy.weekday() + 1
-        if (dayindex == 7):
-            dayindex = 0
+            # 0 is sunday, 6 is saturday
+            dayindex = job.doBy.weekday() + 1
+            if (dayindex == 7):
+                dayindex = 0
 
-        # max possible bar width, based on day of the week
-        maxtrw = self.parent.width / 7 * dayindex
+            # max possible bar width, based on day of the week
+            maxtrw = self.parent.width / 7 * dayindex
 
-        fillx = self.width/2  # fills out the bar
+            fillx = self.width/2  # fills out the bar
 
-        fullbars = 0
-        topbar = 0
-        iteration = 3
-        btmbar = 0
-        # whats going on? it works tho
-        totalspacing = mw * 0.03
-        spacing = totalspacing + totalspacing
-        bh = (mw-spacing-spacing)/3.  # bar height
+            fullbars = 0
+            topbar = 0
+            iteration = 3
+            btmbar = 0
+            # whats going on? it works tho
+            totalspacing = mw * 0.03
+            spacing = totalspacing + totalspacing
+            bh = (mw-spacing-spacing)/3.  # bar height
 
-        ##################
-        # h1 = self.y+self.height/2-mh/2 # make btm flush with main indicator
-        jlos = bh + spacing  # job line offset
-        bos = self.height/2-mh/2  # base offset
+            ##################
+            # h1 = self.y+self.height/2-mh/2 # make btm flush with main indicator
+            jlos = bh + spacing  # job line offset
+            bos = self.height/2-mh/2  # base offset
 
-        if (trw >= maxtrw):
-            # if can't fit
-            btmbar = maxtrw
-            remain = trw - maxtrw  # width of remain trw
-            fullbars = int(remain/self.parent.width)  # number of full bars
-            topbar = remain - fullbars*self.parent.width   # width of top bar
+            if (trw >= maxtrw):
+                # if can't fit
+                btmbar = maxtrw
+                remain = trw - maxtrw  # width of remain trw
+                fullbars = int(remain/self.parent.width)  # number of full bars
+                topbar = remain - fullbars*self.parent.width   # width of top bar
 
-            iteration = fullbars + 1    # number of full bars + top bar
-            # this is if can't fit.
-            # handle btmbar later
+                iteration = fullbars + 1    # number of full bars + top bar
+                # this is if can't fit.
+                # handle btmbar later
 
-            for x in range(1, iteration+1):
-                if (x == iteration):
-                    # dont draw full bars. draw last(top) bar.
-                    insgrp.add(Color(self.parent.cl[ri][0],
-                                  self.parent.cl[ri][1],
-                                  self.parent.cl[ri][2],))
-                    insgrp.add(Rectangle(size=(topbar,
-                                            bh),
-                                      pos=(self.parent.width - topbar,
-                                           self.y + bos + 
-                                           (job.line - 1) * jlos +
-                                           self.parent.height / 5. * x)))
+                for x in range(1, iteration+1):
+                    if (x == iteration):
+                        # TODO: when bars extend across months
+                        # dont draw full bars. draw last(top) bar.
+                        insgrp.add(Color(job.clr[0],
+                                      job.clr[1],
+                                      job.clr[2],))
+                        insgrp.add(Rectangle(size=(topbar,
+                                                bh),
+                                          pos=(self.parent.width - topbar,
+                                               self.y + bos +
+                                               (job.line - 1) * jlos +
+                                               self.parent.height / 5. * x)))
+                    else:
+                        # draw full bars
+                        insgrp.add(Color(job.clr[0],
+                                      job.clr[1],
+                                      job.clr[2],))
+                        insgrp.add(Rectangle(size=(self.parent.width,
+                                                bh),
+                                          pos=(0,
+                                               self.y + bos +
+                                               (job.line - 1) * jlos +
+                                               self.parent.height / 5. * x)))
+
+            btmbar = trw
+            if (btmbar > maxtrw):
+                btmbar = maxtrw
+            insgrp.add(Color(job.clr[0],
+                          job.clr[1],
+                          job.clr[2],))
+
+            if (job.timeReq > 0):  # if job requires time
+                # TODO: timeReq can be used for events? change this to
+                #       use a var like jobType or something
+                if (job.doBy.floor('day') <= arrow.get(job.doBy.timestamp - job.timeReq)):
+                    # TODO: figure out why I wrote this, too tired for now
+                    fillx = 0
+                    xpos = self.center_x-btmbar
+                    # dont fill
                 else:
-                    # draw full bars
-                    insgrp.add(Color(self.parent.cl[ri][0],
-                                  self.parent.cl[ri][1],
-                                  self.parent.cl[ri][2],))
-                    insgrp.add(Rectangle(size=(self.parent.width,
+                    xpos = self.x-btmbar
+
+                insgrp.add(Rectangle(size=(btmbar+fillx,
                                             bh),
-                                      pos=(0,
-                                           self.y + bos + 
-                                           (job.line - 1) * jlos +
-                                           self.parent.height / 5. * x)))
+                                      pos=(xpos,
+                                           self.y + bos +
+                                           (job.line - 1) * jlos)))
+                                       # h1)))
 
-        btmbar = trw
-        if (btmbar > maxtrw):
-            btmbar = maxtrw
-        insgrp.add(Color(self.parent.cl[ri][0],
-                      self.parent.cl[ri][1],
-                      self.parent.cl[ri][2],))
-
-        if (job.timeReq > 0):  # if job requires time
-            # TODO: timeReq can be used for events? change this to
-            #       use a var like jobType or something
-            if (job.doBy.floor('day') <= arrow.get(job.doBy.timestamp - job.timeReq)):
-                # TODO: figure out why I wrote this, too tired for now
-                fillx = 0
-                xpos = self.center_x-btmbar
-                # dont fill
-            else:
-                xpos = self.x-btmbar
-            
-            insgrp.add(Rectangle(size=(btmbar+fillx,
-                                        bh),
-                                  pos=(xpos,
-                                       self.y + bos +
-                                       (job.line - 1) * jlos)))
-                                   # h1)))
-
-            # old method before March 16
-            #insgrp.add(Rectangle(size=(btmbar+fillx, 
-            #                        bh),
-            #                  pos=(self.x-btmbar,
-            #                       self.y + bos +
-            #                       (job.line - 1) * jlos)))
-            #                       # h1)))
+                # old method before March 16
+                #insgrp.add(Rectangle(size=(btmbar+fillx,
+                #                        bh),
+                #                  pos=(self.x-btmbar,
+                #                       self.y + bos +
+                #                       (job.line - 1) * jlos)))
+                #                       # h1)))
 
 
 class FirstScreen(Screen):
@@ -694,23 +738,19 @@ class SecondScreen(Screen):
 
 class CalApp(App):
     # top info area
+    date = arrow.get(tz.tzlocal())
+    cy = date.year # current year
+    cm = date.month # current month
     dayinfo = DayDisplay(size_hint=(1,0.03))
+    monthinfo = MonthDisplay(size_hint=(1,0.08))
         
     # main window area
-    calmain = CalGame(cols=7, size_hint=(1, .55))
+    calmain = CalGame(cols=7, size_hint=(1, .55), dater=date)
     calmain.size = (Window.width, Window.height * 0.95)
     calmain.pos = (0, Window.height - Window.height * 0.95)
 
-    asdf = 1
-
     def delayed(self, dt):
-        self.calmain.makemarks()
-
-    def asd2(self):
-        print(self.asdf)
-        # self.sm = '2'
-        # print(str(sm))
-        # self.calmain.makemars()
+        self.calmain.makemarks(self.cy, self.cm)
 
     def build(self):
         sm = ScreenManager()
@@ -719,12 +759,16 @@ class CalApp(App):
 
         flo = FloatLayout()
         blo = BoxLayout(orientation='vertical')
+        blo.add_widget(self.monthinfo)
         blo.add_widget(self.dayinfo)
         blo.add_widget(self.calmain)
-        ewb = EWB(text='btn0', screeninput=sm,size_hint=(1,.45))
-        ewb1 = EWB(text='btn1', screeninput=sm)
-        ewb2 = EWB(text='btn2', screeninput=sm)
-        eb = BtnArea(cols=3, size_hint=(1, .45))
+        curryr = arrow.get(tz.tzlocal()).year
+        currmnth = arrow.get(tz.tzlocal()).month
+
+        ewb = EWB(text='btn0', screeninput=sm, shift=-1)
+        ewb1 = EWB(text='btn0', screeninput=sm, shift=0)
+        ewb2 = EWB(text='btn0', screeninput=sm, shift=1)
+        eb = BtnArea(cols=3, size_hint=(1, .45), date=self.date)
         # ewb.size = (100,100) # why do i need this??
         blo.add_widget(eb)
         eb.add_widget(ewb)
@@ -741,7 +785,7 @@ class CalApp(App):
         with self.calmain.canvas:
             Rectangle(pos=(0, 0), size=Window.size)
 
-        self.calmain.kivycalpop()
+        self.calmain.kivycalpop(curryr, currmnth)
         Clock.schedule_once(self.delayed, 1)  # TODO: refine delay
         return sm
 
@@ -797,6 +841,7 @@ class Job:
         # self.doBy = datetime.datetime(2016, 1, 27, 20, 46, 43, 0, None)
         self.job_id = job_id
         self.line = 0
+        self.clr = None
 
     # time left (calculated from current time)
     def timeLeft(self):
@@ -877,6 +922,20 @@ def initdbclass():
     q = """
     INSERT INTO wt VALUES('test new on 25th','86400',
                           STR_TO_DATE('2016/03/25 08:00:00',
+
+                                      '%Y/%m/%d %T'), NULL);
+    """
+    db.query(q)
+    q = """
+    INSERT INTO wt VALUES('new item in april','604800',
+                          STR_TO_DATE('2016/04/02 08:00:00',
+                                      '%Y/%m/%d %T'), NULL);
+    """
+    db.query(q)
+
+    q = """
+    INSERT INTO wt VALUES('second new item in april','86400',
+                          STR_TO_DATE('2016/04/15 08:00:00',
                                       '%Y/%m/%d %T'), NULL);
     """
     db.query(q)
